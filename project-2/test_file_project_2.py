8332ffc9045988a91eeb912ce811904990015165
Ana Flávia Souza <ana.fsouza.santos@gmail.com>
2025-07-29 10:43:30 -0300

    test: add new dummy function

--- project-2/test_file_project_2.py
+++ project-2/test_file_project_2.py
@@ -1,2 +1,6 @@
 def dummy_function():
     return "This is a dummy function for testing purposes."
+
+def dummy_function_with_args(arg1, arg2):
+    """This function takes two arguments and returns their sum."""
+    return arg1 + arg2
