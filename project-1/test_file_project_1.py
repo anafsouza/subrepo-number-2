ef151372752fad5ba2d6b5a3d5094c9b2509a581
Ana Flávia Souza <ana.fsouza.santos@gmail.com>
2025-07-29 11:21:48 -0300

    test: add new function to project 1

--- project-1/test_file_project_1.py
+++ project-1/test_file_project_1.py
@@ -14,3 +14,20 @@ def fibbonacci_number(n):
         return 1
     else:
         return fibbonacci_number(n - 1) + fibbonacci_number(n - 2)
+    
+def factorial(n):
+    """Compute the factorial of a number.
+
+    Args:
+        n: The number to compute the factorial for.
+
+    Returns:
+        The factorial of n.
+    """
+    
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
